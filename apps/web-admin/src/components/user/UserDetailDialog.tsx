0d69c84977dd7b8119aee1cf896f15f1323941aa
zouhang <1102639563@qq.com>
2024-07-11 00:21:50 +0800

    chore: update logic

--- apps/web-admin/src/components/user/UserDetailDialog.tsx
+++ apps/web-admin/src/components/user/UserDetailDialog.tsx
@@ -1,10 +1,26 @@
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router'
+
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { useAuth } from '@/context/ProviderAuth'
 
 export function UserDetailDialog({ user, onClose }: { user: any; onClose: any }) {
+  const { id } = useParams()
+  const { authClient } = useAuth()
+
+  const { data: referral } = useQuery({
+    queryKey: ['get-user-referral', id, user?.id],
+    queryFn: async () => {
+      return await authClient?.admin.getUserReferral(id!, user?.id)
+    },
+    enabled: !!user,
+  })
+
   if (!user) {
     return null
   }
+
   return (
     <Dialog open={!!user} onOpenChange={(open) => !open && onClose()}>
       <DialogContent className="max-w-2xl">
@@ -19,6 +35,16 @@ export function UserDetailDialog({ user, onClose }: { user: any; onClose: any })
                 <div className="flex-1">{user[key]}</div>
               </li>
             ))}
+            {referral && (
+              <>
+                <li className="flex items-center">
+                  <div className="w-1/3 text-muted-foreground capitalize">Referral</div>
+                  <div className="flex-1">
+                    refee1Count: {referral.refee1Count} <br /> refee2Count: {referral.refee2Count}
+                  </div>
+                </li>
+              </>
+            )}
           </ul>
         </div>
         <DialogFooter>
