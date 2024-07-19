78509c76c28b82775eb35dbcd652c2c7bd39cc9e
zouhang <1102639563@qq.com>
2024-07-19 23:40:07 +0800

    chore: add query helper (#17)
    Co-authored-by: Haiyang Wang <whywanghai@gmail.com>

---
+++ packages/sdk-react/utils/request.ts
@@ -0,0 +1,54 @@
+interface Params {
+  [key: string]: any
+}
+
+interface Headers {
+  [key: string]: string
+}
+
+export const get = async <T>(url: string, params: Params = {}, headers: Headers = {}): Promise<T> => {
+  // 构建查询字符串
+  const queryString = new URLSearchParams(params).toString()
+  const response = await fetch(`${url}?${queryString}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+  })
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`)
+  }
+
+  const json = await response.json()
+
+  if (json.success === false) {
+    throw new Error(json.msg || json.message)
+  }
+
+  return json
+}
+
+export const post = async <T>(url: string, data: any = {}, headers: Headers = {}): Promise<T> => {
+  const response = await fetch(`${url}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+    body: JSON.stringify(data),
+  })
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`)
+  }
+
+  const json = await response.json()
+
+  if (json.success === false) {
+    throw new Error(json.msg || json.message)
+  }
+
+  return json
+}
