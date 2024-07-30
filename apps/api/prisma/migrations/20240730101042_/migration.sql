d323a5347621dac4fd34e5fb135b31682eb1c6fa
callmewhy <whywanghai@gmail.com>
2024-07-30 18:11:08 +0800

    no message

---
+++ apps/api/prisma/migrations/20240730101042_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `referChain` on the `oa_users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "oa_users" DROP COLUMN "referChain";
