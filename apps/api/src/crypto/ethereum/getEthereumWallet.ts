ee016874dbc7e39bba9aa75dba31c366e4a110a7
callmewhy <whywanghai@gmail.com>
2024-10-09 19:03:02 +0800

    fix evm hdwallet

--- apps/api/src/crypto/ethereum/getEthereumWallet.ts
+++ apps/api/src/crypto/ethereum/getEthereumWallet.ts
@@ -1,22 +1,19 @@
 import crypto from 'node:crypto'
 
 import { u8aToHex } from '@polkadot/util'
-import { HDKey, hdKeyToAccount } from 'viem/accounts'
+import { privateKeyToAccount } from 'viem/accounts'
 
 import { WALLET_SEED_SALT } from '../../constants'
 
 export function getEthereumWallet(userId: string) {
   const seedStr = `${WALLET_SEED_SALT}_${userId}`
-  const hash = crypto.createHash('sha256')
-  hash.update(Buffer.from(seedStr))
-  const seed = hash.digest()
-
-  const hdKey = HDKey.fromMasterSeed(seed)
-  const account = hdKeyToAccount(hdKey)
+  const privateKeyHash = crypto.createHash('sha256').update(Buffer.from(seedStr)).digest()
+  const privateKey = u8aToHex(privateKeyHash)
+  const account = privateKeyToAccount(privateKey)
 
   return {
     account,
     walletAddress: account.address,
-    privateKey: u8aToHex(hdKey.privateKey),
+    privateKey,
   }
 }
